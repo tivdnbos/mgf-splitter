8e188c5ca1eeb4d741f14063c9a977320e006f09
tivdnbos <t.vdbossche@hotmail.com>
2021-08-26 14:17:40 +0200

    add argparse

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-mgf_path = "C:\\Users\\tvdbo\\Documents\\Projecten\\ms2rescore-idrates\\mgf\\151222_NJ_Felipe_Y1a.mgf"
+import argparse
 
-with open(mgf_path, "r") as mgf_in:
-    mgf_counter = 0
 
-    f_out = open(mgf_path.replace(".mgf", "_{}.mgf".format(str(mgf_counter).zfill(2))), "w")
-    spectrum_count = 0
-    for line in mgf_in:
-        if "BEGIN IONS" in line:
-            spectrum_count += 1
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mgf_file", help="Path to single mgf file")
+    parser.add_argument("size", help="Max number of spectra per mgf")
+    return parser.parse_args()
 
-            if spectrum_count == 50001:
-                f_out.close()
-                spectrum_count = 0
-                mgf_counter += 1
-                f_out = open(mgf_path.replace(".mgf", "_{}.mgf".format(str(mgf_counter).zfill(2))), "w")
-                print(spectrum_count)
 
-        print(line.rstrip(), file=f_out)
+def main():
 
-f_out.close()
-print(spectrum_count)
+    args = parse_args()
+    mgf_path = args.mgf_file
+    max_size = int(args.size)
+
+    with open(mgf_path, "r") as mgf_in:
+        mgf_counter = 0
+        f_out = open(mgf_path.replace(".mgf", "_{}.mgf".format(str(mgf_counter).zfill(2))), "w")
+        spectrum_count = 0
+        for line in mgf_in:
+            if "BEGIN IONS" in line:
+                spectrum_count += 1
+                if spectrum_count == max_size + 1:
+                    f_out.close()
+                    spectrum_count = 1
+                    mgf_counter += 1
+                    f_out = open(mgf_path.replace(".mgf", "_{}.mgf".format(str(mgf_counter).zfill(2))), "w")
+
+            print(line.rstrip(), file=f_out)
+
+    f_out.close()
+
+
+main()
